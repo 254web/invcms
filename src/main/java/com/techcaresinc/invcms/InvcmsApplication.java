0c6a3676810e8aef59c349ddcd7b6ce28fea6de2
JMchfundi <akidamjaffar@gmail.com>
2020-02-25 16:01:57 +0300

    some few fix

--- src/main/java/com/techcaresinc/invcms/InvcmsApplication.java
+++ src/main/java/com/techcaresinc/invcms/InvcmsApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class InvcmsApplication extends SpringBootServletInitializer {
 
     @RequestMapping(value = "/")
@@ -14,6 +16,10 @@ public class InvcmsApplication extends SpringBootServletInitializer {
         return "index";
     }
 
+    @RequestMapping(value = "/invoice")
+    public String invoice (){
+        return "invoice";
+    }
     public static void main(String[] args) {
         SpringApplication.run(InvcmsApplication.class, args);
     }
